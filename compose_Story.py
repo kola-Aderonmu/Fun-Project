e26f83ffdb9c74a9fa5396307e53f77224d377b9
k@y <54904593+kola-Aderonmu@users.noreply.github.com>
2023-10-03 17:32:20 +0100

    Add files via upload

---
+++ compose_Story.py
@@ -0,0 +1,38 @@
+'''
+MAD LIBs GENERATOR
+--------------------------------------------------------------------------
+'''
+
+
+# User input
+
+noun = input("Choose a noun: ")
+
+p_noun = input("Choose a plural noun: ")
+
+noun2 = input("Choose a noun: ")
+
+place = input("Name a location: ")
+
+adjective = input("Choose a descrption: ")
+
+noun3 = input("Choose a noun: ")
+
+#print a story from the user input.
+# Print a story from the user input
+
+print('------------------------------------------')
+
+print('Be kind to your', noun, '- footed', p_noun)
+
+print('For a duck may be somebody\'s', noun2, ',')
+
+print('Be kind to your', p_noun, 'in', place)
+
+print('Where the weather is always', adjective, '. \n')
+
+print('You may think that is this the', noun3, ',')
+
+print('Well it is.')
+
+print('------------------------------------------')
